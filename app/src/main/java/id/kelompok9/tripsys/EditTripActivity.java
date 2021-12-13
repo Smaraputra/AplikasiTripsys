b37021241ddb9fd101472807748757a9cf020f41
RahSuwijaya <80748618+NgurahSuwijaya@users.noreply.github.com>
2021-12-14 00:14:22 +0800

    erererer

---
+++ app/src/main/java/id/kelompok9/tripsys/EditTripActivity.java
@@ -0,0 +1,14 @@
+package id.kelompok9.tripsys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_trip);
+    }
+}
